44d890413b2678ea6e406eb7c9ab9acbe11dc8ef
Ricardo <rafaelsmythe@hotmail.com>
2018-09-10 11:23:04 -0500

    Color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { MenuPage } from '../pages/menu/menu';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +25,10 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'About', component: AboutPage },
+      { title: 'Contact', component: ContactPage },
+      { title: 'Menu', component: MenuPage }
     ];
 
   }
